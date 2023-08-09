ec0e9f2bb0c5b61844f7f0a699ee1b3b37f54369
unknown <Jorgemathez14@gmail.com>
2023-08-09 05:23:33 -0300

    Footer

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,7 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  persistSession: false, // Configura la persistencia de sesión
+});
 const storage = supabase.storage; // Agrega esta línea para crear una referencia al objeto storage
 
 export { supabase, storage }; // Exporta ambos objetos
